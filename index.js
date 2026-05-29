862ab078b63137a92e2b500917ebb49469b9b490
frzx-cloud <kaua.lima11@escola.pr.gov.br>
2026-05-29 12:00:51 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+// npm init
+// npm i express
+// instalar extensão RapidAPI Client
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.json())
+const fs = require('fs')
+
+app.post("/clientes/cadastro", (req,res) => {
+    const cliente = req.body
+    if (!cliente || Object.keys(cliente).length === 0) {
+        res.status(400).json({resposta: "Body não preenchido"})
+    } else {
+        try {
+            const bd = JSON.parse(fs.readFileSync('bd.json', 'utf8'))
+            bd.push(cliente)
+            fs.writeFileSync('bd.json', JSON.stringify(bd), 'utf8')
+            res.status(200).json({resposta: "Cliente cadastrado com sucesso!"})
+        } catch(error) {
+            res.status(500).json({resposta: error.message})
+        }
+    }    
+})
+
+app.get("/ola", (req, res)=>{
+    res.json({resposta: "Olá Mundo!"})
+})
+
+app.listen(port, ()=>{
+    console.log("API rodando na porta " + port)
+})
